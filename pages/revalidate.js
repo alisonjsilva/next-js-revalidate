ceea17b27acefa9a045c8a1a2ba89615bc2723ea
Alison Silva <alison.silva@wundermanthompson.com>
2023-01-24 08:41:42 +0000

    trying node-cache

--- pages/revalidate.js
+++ pages/revalidate.js
@@ -1,11 +1,15 @@
 import getFile from "@/data/readFiles";
+import NodeCache from "node-cache";
 
+const cache = new NodeCache();
 
 export default function Index({ data }) {
+    console.log(data);
     return (
         <div>
             This page needs to be revalidate
-            <p>{data}</p>
+            <p>{data.random}</p>
+            <p>{data.cacheResult.value}</p>
         </div>
     )
 }
@@ -19,9 +23,32 @@ export async function getStaticProps(context) {
     const f = await getFile();
     console.log(f);
 
+    const cacheResult = await handleCache('routes-table');
+    console.log(cacheResult);
+
+    // const randomToCache = getRandomInt(500000);
+
     return {
         props: {
-            data: getRandomInt(300000)
+            data: { random: getRandomInt(300000), cacheResult }
         }
     }
+}
+
+async function handleCache(key) {
+
+
+    let value = cache.get(key);
+    console.log('value', value);
+    if (value === undefined) {
+        value = await getFile();
+        console.log('put in cache');
+        const success = cache.set(key, value, 100000);
+        if (success)
+            console.log('success in put cache');
+    } else {
+        console.log('from cache');
+    }
+
+    return value;
 }
